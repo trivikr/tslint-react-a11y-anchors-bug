85070ffa38297106210180bb54e89791e7be1356
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-11-30 14:44:38 -0800

    Update README and code with unsuccessful repro

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tslint-react-a11y-anchors-bug
-Minimal repository to repro a bug with TSLint react-a11y-anchors rule
+
+This repository tries to reproduce bug with `tslint-microsoft-contrib` rule `react-a11y-anchors` when `href="tel:1-234-567-8900"` is passed, but not successfully able to do so.
+
+- run `npm install`
+- run `npm run build`
+
+The build is successful, but it fails on one of my private projects.
--- app.tsx
+++ app.tsx
@@ -2,13 +2,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
-    <div>
-      {/*<a href="tel:1-234-567-8900">Link with tel in href</a>*/}
-      <a href="/link">Link with URL in href</a>
-    </div>
-    <div>
-      <a href="/link">Link with URL in href</a>
-    </div>
+    {/* react-a11y-anchors fails in one of my private projects,
+        but works fine in this repo */}
+    <a href="tel:1-234-567-8900">Link with tel in href</a>
+    <a href="/link">Link with URL in href</a>
   </div>,
   document.getElementById("root")
 );
